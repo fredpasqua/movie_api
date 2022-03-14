ba6349d9832f4ad6850ea18da0d0e69f606f2bc4
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-03-14 18:25:34 -0400

    passport update

--- passport.js
+++ passport.js
@@ -1,4 +1,4 @@
-const passport = require('@types/passport'),
+const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
